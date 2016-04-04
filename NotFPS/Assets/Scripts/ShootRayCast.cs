88f3ccbb0d24855bfc535fc2b7f49a120f6f9f7e
Arun009 <arun.pattu9@gmail.com>
2016-04-04 03:02:07 -0400

    Added Push and Pull Behaviors. Left Click - Object gets pushed away from the character if the Object mass is lower then Characters mass                   - Character gets pushed away from the object if the Object mass is higher than the Characters mass Right Click - Object gets pulled towards the character if the Object mass is lower then Characters mass                      - Character gets pulled towards the object if the Object mass is higher than the Characters mass

---
+++ NotFPS/Assets/Scripts/ShootRayCast.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootRayCast : MonoBehaviour {
+
+    public GameObject fPSController;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pushOrPullForLeftMouseButton();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            pushOrPullForRightMouseButton();
+        }
+    }
+
+    void pushOrPullForLeftMouseButton()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if(Physics.Raycast(transform.position, fwd, out hit, float.MaxValue))
+        {
+            
+            if(hit.transform.tag.Equals("Metal"))
+            {
+                Debug.Log("hit " + hit.transform.tag);
+                if(hit.transform.GetComponent<Rigidbody>().mass < fPSController.GetComponent<Rigidbody>().mass)
+                {
+                    hit.transform.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+                }
+                else
+                {
+                    fPSController.GetComponent<Rigidbody>().transform.Translate(-transform.forward * 5);
+                }
+            }
+        }
+    }
+
+    void pushOrPullForRightMouseButton()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, out hit, float.MaxValue))
+        {
+
+            if (hit.transform.tag.Equals("Metal"))
+            {
+                if (hit.transform.GetComponent<Rigidbody>().mass < fPSController.GetComponent<Rigidbody>().mass)
+                {
+                    hit.transform.GetComponent<Rigidbody>().AddForce(-transform.forward * 500);
+                }
+                else
+                {
+                    fPSController.GetComponent<Rigidbody>().transform.Translate(transform.forward * 5);
+                }
+            }
+        }
+    }
+}
