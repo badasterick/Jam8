45a92e0b9ed7fa05778b87d43ebcb4b4207d925e
steinfernandez <steinfernandez@gmail.com>
2016-04-10 15:25:19 -0400

    switch and deadlylaser prefabs.

---
+++ NotFPS/Assets/SwitchScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScript : MonoBehaviour {
+	public GameObject gate;
+	private bool pressed;
+	// Use this for initialization
+	void Start () 
+	{
+		pressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter()
+	{
+		if (pressed == false) 
+		{
+			gate.transform.position = gate.transform.position + new Vector3 (0, 5, 0);
+			pressed = true;
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		if (pressed == true) 
+		{
+			gate.transform.position = gate.transform.position + new Vector3 (0, -5, 0);
+			pressed = false;
+		}
+	}
+}
